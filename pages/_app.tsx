ddbb6ac53351c59b068317114fb45c7212e77872
sungyun <sungyun5423@gmail.com>
2022-05-07 14:22:31 +0900

    feat: react-query setting

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,17 @@
 import type { AppProps } from 'next/app';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 
 import GlobalStyle from '../styles/global.style';
 
+const queryClient = new QueryClient();
+
 const app = ({ Component, pageProps }: AppProps) => (
-  <>
+  <QueryClientProvider client={queryClient}>
     <GlobalStyle />
     <Component {...pageProps} />
-  </>
+    <ReactQueryDevtools />
+  </QueryClientProvider>
 );
 
 export default app;
