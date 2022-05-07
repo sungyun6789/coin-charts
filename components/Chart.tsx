660c82c19fb1d2cb086a2f42d183fa924bf5e95e
sungyun <sungyun5423@gmail.com>
2022-05-07 13:12:00 +0900

    feat: 필요 없는 부분 삭제

--- components/Chart.tsx
+++ components/Chart.tsx
@@ -1,27 +1,14 @@
-import { CartesianGrid, Legend, Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
+import { Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
 
 import mock from '../mock';
 
 const Chart = () => {
   return (
-    <LineChart
-      width={500}
-      height={300}
-      data={mock}
-      margin={{
-        top: 5,
-        right: 30,
-        left: 20,
-        bottom: 5,
-      }}
-    >
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="name" />
+    <LineChart width={1000} height={500} data={mock}>
+      <XAxis dataKey={undefined} />
       <YAxis />
       <Tooltip />
-      <Legend />
       <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
-      <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
     </LineChart>
   );
 };
