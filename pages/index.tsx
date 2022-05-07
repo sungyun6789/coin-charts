5c9af80643c44521d1a376931cbf98a9848dde73
sungyun <sungyun5423@gmail.com>
2022-05-07 20:06:28 +0900

    feat: 업비트에서 KRW-BTC 정보 가져오는 로직 구현

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,45 @@
 import styled from '@emotion/styled';
+import axios from 'axios';
 import dynamic from 'next/dynamic';
+import { useQuery } from 'react-query';
 
 const Chart = dynamic(() => import('../components/Chart'), { ssr: false });
+
+interface UpbitResponseModel {
+  market: string;
+  candle_date_time_utc: string;
+  candle_date_time_kst: string;
+  opening_price: number;
+  high_price: number;
+  low_price: number;
+  trade_price: number;
+  timestamp: number;
+  candle_acc_trade_price: number;
+  candle_acc_trade_volume: number;
+  prev_closing_price: number;
+  change_price: number;
+  change_rate: number;
+}
+
+const Index = () => {
+  const { data } = useQuery(
+    ['/candles/days?market=KRW-BTC'],
+    async () =>
+      (await axios.get<UpbitResponseModel>('https://api.upbit.com/v1/candles/days?market=KRW-BTC&count=100')).data,
+    {
+      refetchOnMount: false,
+      refetchOnWindowFocus: false,
+    },
+  );
+
+  return (
+    <Main>
+      <Chart />
+    </Main>
+  );
+};
+
+export default Index;
 
 const Main = styled.main`
   width: 100vw;
@@ -11,13 +49,3 @@ const Main = styled.main`
   justify-content: center;
   align-items: center;
 `;
-
-const Index = () => {
-  return (
-    <Main>
-      <Chart />
-    </Main>
-  );
-};
-
-export default Index;
