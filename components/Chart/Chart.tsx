8cc8a49a45fecab18cdbb6031c7429f93adfee31
sungyun <sungyun5423@gmail.com>
2022-05-07 21:30:13 +0900

    refactor: 데이터를 props로 넘기지 않고 컴포넌트 내에서 처리하도록 함

--- components/Chart/Chart.tsx
+++ components/Chart/Chart.tsx
@@ -1,12 +1,35 @@
+import axios from 'axios';
+import { useQuery } from 'react-query';
 import { Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
 
-import type { UpbitResponseModel } from '../../pages';
-
-interface Props {
-  data: UpbitResponseModel[] | undefined;
+export interface UpbitResponseModel {
+  market: string;
+  candle_date_time_utc: string;
+  candle_date_time_kst: string;
+  opening_price: number;
+  high_price: number;
+  low_price: number;
+  trade_price: number;
+  timestamp: number;
+  candle_acc_trade_price: number;
+  candle_acc_trade_volume: number;
+  prev_closing_price: number;
+  change_price: number;
+  change_rate: number;
 }
 
-const Chart = ({ data }: Props) => {
+const Chart = () => {
+  const { data } = useQuery(
+    ['/candles/days?market=KRW-BTC'],
+    async () =>
+      (await axios.get<UpbitResponseModel[]>('https://api.upbit.com/v1/candles/days?market=KRW-BTC&count=100')).data,
+    {
+      refetchOnMount: false,
+      refetchOnWindowFocus: false,
+      onSuccess: (value) => value.sort().reverse(),
+    },
+  );
+
   return (
     <LineChart width={1000} height={500} data={data}>
       <XAxis dataKey="candle_date_time_utc" />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,41 +1,12 @@
 import styled from '@emotion/styled';
-import axios from 'axios';
 import dynamic from 'next/dynamic';
-import { useQuery } from 'react-query';
 
 const Chart = dynamic(() => import('../components/Chart/Chart'), { ssr: false });
 
-export interface UpbitResponseModel {
-  market: string;
-  candle_date_time_utc: string;
-  candle_date_time_kst: string;
-  opening_price: number;
-  high_price: number;
-  low_price: number;
-  trade_price: number;
-  timestamp: number;
-  candle_acc_trade_price: number;
-  candle_acc_trade_volume: number;
-  prev_closing_price: number;
-  change_price: number;
-  change_rate: number;
-}
-
 const Index = () => {
-  const { data } = useQuery(
-    ['/candles/days?market=KRW-BTC'],
-    async () =>
-      (await axios.get<UpbitResponseModel[]>('https://api.upbit.com/v1/candles/days?market=KRW-BTC&count=100')).data,
-    {
-      refetchOnMount: false,
-      refetchOnWindowFocus: false,
-      onSuccess: (value) => value.sort().reverse(),
-    },
-  );
-
   return (
     <Main>
-      <Chart data={data} />
+      <Chart />
     </Main>
   );
 };
