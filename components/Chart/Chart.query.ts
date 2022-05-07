e338d2d601bca77e1fa497d02682787349cd07be
sungyun <sungyun5423@gmail.com>
2022-05-07 21:36:27 +0900

    refactor: type, query 로직 분리

---
+++ components/Chart/Chart.query.ts
@@ -0,0 +1,18 @@
+import axios from 'axios';
+import { useQuery } from 'react-query';
+
+import type { UpbitResponseModel } from './Chart.type';
+
+const getBtcData = async () => {
+  const { data } = await axios.get<UpbitResponseModel[]>(
+    'https://api.upbit.com/v1/candles/days?market=KRW-BTC&count=100',
+  );
+  return data;
+};
+
+export const useBtcDataQuery = () =>
+  useQuery(['/candles/days?market=KRW-BTC'], getBtcData, {
+    refetchOnMount: false,
+    refetchOnWindowFocus: false,
+    onSuccess: (value) => value.sort().reverse(),
+  });
--- components/Chart/Chart.tsx
+++ components/Chart/Chart.tsx
@@ -1,34 +1,9 @@
-import axios from 'axios';
-import { useQuery } from 'react-query';
 import { Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
 
-export interface UpbitResponseModel {
-  market: string;
-  candle_date_time_utc: string;
-  candle_date_time_kst: string;
-  opening_price: number;
-  high_price: number;
-  low_price: number;
-  trade_price: number;
-  timestamp: number;
-  candle_acc_trade_price: number;
-  candle_acc_trade_volume: number;
-  prev_closing_price: number;
-  change_price: number;
-  change_rate: number;
-}
+import { useBtcDataQuery } from './Chart.query';
 
 const Chart = () => {
-  const { data } = useQuery(
-    ['/candles/days?market=KRW-BTC'],
-    async () =>
-      (await axios.get<UpbitResponseModel[]>('https://api.upbit.com/v1/candles/days?market=KRW-BTC&count=100')).data,
-    {
-      refetchOnMount: false,
-      refetchOnWindowFocus: false,
-      onSuccess: (value) => value.sort().reverse(),
-    },
-  );
+  const { data } = useBtcDataQuery();
 
   return (
     <LineChart width={1000} height={500} data={data}>
