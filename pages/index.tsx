5e2d16d4b1550b377ebdf7e646606a360ee8dddb
sungyun <sungyun5423@gmail.com>
2022-05-10 00:40:50 +0900

    style: 차트 관리하는 헤더 스타일 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,16 +15,20 @@ const Index = () => {
 
   return (
     <>
-      최근
-      <input value={count} onChange={(e) => setCount(+e.target.value)} />
-      일의 KRW-BTC 가격
-      <button onClick={() => refetch()}>조회</button>
-      <div>
+      <Header>
         <select onChange={(e) => setChartType(e.target.value as '날짜선택' | '현재가격')}>
           <option value="날짜선택">날짜 선택</option>
           <option value="현재가격">현재 가격</option>
         </select>
-      </div>
+      </Header>
+      {chartType === '날짜선택' && (
+        <SelectDateWrapper>
+          최근
+          <input value={count} onChange={(e) => setCount(+e.target.value)} />
+          일동안의 KRW-BTC 가격
+          <button onClick={() => refetch()}>조회</button>
+        </SelectDateWrapper>
+      )}
       <Main>{chartType === '날짜선택' ? <LChart count={count} /> : <BChart />}</Main>
     </>
   );
@@ -43,5 +47,52 @@ const Main = styled.main`
   .recharts-cartesian-axis-ticks {
     font-size: 11px;
     width: 200px;
+  }
+`;
+
+const Header = styled.header`
+  display: flex;
+  justify-content: center;
+  height: 50px;
+  margin: 20px;
+
+  select {
+    width: 200px;
+    height: 100%;
+    font-size: 16px;
+    border: 1px solid #e0e0e0;
+    outline: none;
+  }
+`;
+
+const SelectDateWrapper = styled.article`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  input {
+    width: 60px;
+    height: 40px;
+    font-size: 16px;
+    text-align: right;
+    margin: 0 3px;
+    border: 1px solid #e0e0e0;
+    font-size: 16px;
+    outline: none;
+    box-sizing: border-box;
+  }
+
+  button {
+    width: 60px;
+    height: 40px;
+    margin: 0 3px;
+    background-color: white;
+    border: 1px solid #e0e0e0;
+    font-size: 16px;
+    cursor: pointer;
+    transition: 0.3s;
+    :hover {
+      background-color: #f0f0f0;
+    }
   }
 `;
