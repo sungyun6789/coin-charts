6d885169153e48c42872333b815c100aabb2ecaa
sungyun <sungyun5423@gmail.com>
2022-05-09 21:14:26 +0900

    feat: 차트 스위칭을 위한 기본 작업

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,7 @@ import { useBtcDataQuery } from '../components/Chart/Chart.query';
 const Chart = dynamic(() => import('../components/Chart'), { ssr: false });
 
 const Index = () => {
+  const [isCurrentPrice, setIsCurrentPrice] = useState(false);
   const [count, setCount] = useState(10);
   const { data, refetch } = useBtcDataQuery(count);
 
@@ -17,9 +18,11 @@ const Index = () => {
       <input value={count} onChange={(e) => setCount(+e.target.value)} />
       일의 KRW-BTC 가격
       <button onClick={() => refetch()}>조회</button>
-      <Main>
-        <Chart btcData={data} />
-      </Main>
+      <div>
+        현재가격보기
+        <input type="checkbox" onChange={(e) => setIsCurrentPrice(e.target.checked)} />
+      </div>
+      <Main>{isCurrentPrice ? <div /> : <Chart btcData={data} />}</Main>
     </>
   );
 };
