c6f1519451ca3246f9bfb7af56073521c232032e
sungyun <sungyun5423@gmail.com>
2022-05-10 23:56:44 +0900

    feat: websocket 사용해서 실시간 통신하도록 구현, 차트 주석 처리

--- components/Chart/BChart.tsx
+++ components/Chart/BChart.tsx
@@ -1,20 +1,35 @@
-import styled from '@emotion/styled';
-import { Bar, BarChart, Tooltip, XAxis, YAxis } from 'recharts';
+import { useEffect, useState } from 'react';
 
-import { useBtcCurrentDataQuery } from './Chart.query';
+import styled from '@emotion/styled';
+// import { Bar, BarChart, Tooltip, XAxis, YAxis } from 'recharts';
+
+// import { useBtcCurrentDataQuery } from './Chart.query';
 
 const BChart = () => {
-  const { data } = useBtcCurrentDataQuery();
+  // const { data } = useBtcCurrentDataQuery();
+  const [socketData, setSocketData] = useState<any>();
+
+  useEffect(() => {
+    const socket = new WebSocket('wss://api.upbit.com/websocket/v1');
+    socket.binaryType = 'arraybuffer';
+    socket.onopen = () => socket.send(JSON.stringify([{ ticket: 'ticket' }, { type: 'ticker', codes: ['KRW-BTC'] }]));
+    socket.onmessage = (event) => {
+      const enc = new TextDecoder('utf-8');
+      const arr = new Uint8Array(event.data);
+      setSocketData(JSON.parse(enc.decode(arr)));
+    };
+    return () => socket.close();
+  }, []);
 
   return (
     <ChartWrapper>
-      <div>현재 가격: {data?.[0].trade_price.toLocaleString('ko-KR')}원</div>
-      <BarChart width={250} height={500} data={data}>
+      <div>현재 가격: {socketData?.trade_price.toLocaleString('ko-KR')}원</div>
+      {/* <BarChart width={250} height={500} data={socketData}>
         <Tooltip />
         <XAxis dataKey="market" />
         <YAxis dataKey="trade_price" />
         <Bar dataKey="trade_price" fill="#8884d8" />
-      </BarChart>
+      </BarChart> */}
     </ChartWrapper>
   );
 };
